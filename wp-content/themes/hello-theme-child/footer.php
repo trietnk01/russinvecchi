31afefd9977c4c31d210b7954eded2a9737b1bcf
unknown <trietnk01@gmail.com>
2021-11-01 16:10:04 +0700

    Update code 01-11-2021-16h00

--- wp-content/themes/hello-theme-child/footer.php
+++ wp-content/themes/hello-theme-child/footer.php
@@ -1,16 +1,32 @@
-<footer>
-	<div class="col-logo">
-		<a href="javascript:void(0);"><img class="logo-footer" src="<?php echo wp_get_upload_dir()['url'] . '/logo.png'; ?>" alt="<?php echo get_bloginfo('name'); ?>"></a>
-		<div>
+<footer class="ps-8 pe-8 pt-6 pb-6">
+	<div class="col-logo pe-4">
+		<p style="text-align: center;"><a href="#"><img class="logo-footer" src="http://russinvecchi.dienkim/wp-content/uploads/logo.png" alt=""></a></p>
+		<p style="text-align: center;">
 			Russin & Vecchi was founded in Asia over 50 years ago to serve emerging economies.
 			From entry strategy to operations, we provide creative and alternative legal, business solutions in many fields. We bring practical and commercial answers to our clients.
-		</div>
-		<div>
+		</p>
+		<p style="text-align: center;font-size:8px">
 			All Rights Reserved. &copy; Russin & Vecchi 2020 | Website by Digital Mekong
+		</p>
+	</div>
+	<div class="col-white"></div>
+	<div class="col-card ps-4 pe-4">
+		<p style="text-align: center;">DISCLAMER</p>
+		<p style="text-align: center;">PRIVATE POLICY</p>
+		<p style="text-align: center;">RELATIONSHIPS BETWEEN LAWYERS</p>
+	</div >
+	<div class="col-white"></div>
+	<div class="col-contact ps-4">
+		<a href="javascript:void(0);" class="contact-footer mb-2">CONTACT</a>
+		<div class="address-phone-footer">
+			<p>205 Tun Hwa N. Road</p>
+			<p>9th Floor, Bank Tower</p>
+			<p>Taipei, Taiwan</p>
+			<p>T: 886-2-2712-8956/2713-6110</p>
+			<p>F: 886-2-2713-4711</p>
+			<p>russin@russinvecchi.com.tw</p>
 		</div>
 	</div>
-	<div class="col-card"></div>
-	<div class="col-contact"></div>
 </footer>
 <?php
 /**
