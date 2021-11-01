b70a4c489600b42ece6159fcec5757306c199106
unknown <trietnk01@gmail.com>
2021-11-01 12:40:48 +0700

    Update code 01-11-2021-13h00

---
+++ wp-content/themes/hello-theme-child/footer.php
@@ -0,0 +1,28 @@
+<footer>
+	<div class="col-logo">
+		<a href="javascript:void(0);"><img class="logo-footer" src="<?php echo wp_get_upload_dir()['url'] . '/logo.png'; ?>" alt="<?php echo get_bloginfo('name'); ?>"></a>
+		<div>
+			Russin & Vecchi was founded in Asia over 50 years ago to serve emerging economies.
+			From entry strategy to operations, we provide creative and alternative legal, business solutions in many fields. We bring practical and commercial answers to our clients.
+		</div>
+		<div>
+			All Rights Reserved. &copy; Russin & Vecchi 2020 | Website by Digital Mekong
+		</div>
+	</div>
+	<div class="col-card"></div>
+	<div class="col-contact"></div>
+</footer>
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the body & html closing tags.
+ *
+ * @package HelloElementor
+ */
+wp_footer();
+?>
+
+</body>
+
+</html>
