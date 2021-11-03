f630a274ecd2ea3cad74f05aa2fa4485a57c62c9
unknown <trietnk01@gmail.com>
2021-11-03 08:45:05 +0700

    Update code 03-11-2021-09h00

--- wp-content/themes/hello-theme-child/footer.php
+++ wp-content/themes/hello-theme-child/footer.php
@@ -1,32 +1,34 @@
-<footer class="ps-8 pe-8 pt-8 pb-8">
-	<div class="col-logo pe-4">
-		<p style="text-align: center;"><a href="#"><img class="logo-footer" src="http://preprod.itsabeautifulway.com/russinvecchi.com/wp-content/uploads/logo.png" alt=""></a></p>
-		<p style="text-align: center;">
-			Russin & Vecchi was founded in Asia over 50 years ago to serve emerging economies.
-			From entry strategy to operations, we provide creative and alternative legal, business solutions in many fields. We bring practical and commercial answers to our clients.
-		</p>
-		<p style="text-align: center;font-size:8px">
-			All Rights Reserved. &copy; Russin & Vecchi 2020 | Website by Digital Mekong
-		</p>
-	</div>
-	<div class="col-white"></div>
-	<div class="col-card ps-4 pe-4">
-		<div>
-			<p style="text-align: center;"><a href="#">DISCLAMER</a></p>
-			<p style="text-align: center;"><a href="#">PRIVATE POLICY</a></p>
-			<p style="text-align: center;"><a href="#">RELATIONSHIPS BETWEEN LAWYERS</a></p>
+<footer class="pt-8 pb-8">
+	<div class="russin-container">
+		<div class="col-logo pe-4">
+			<p style="text-align: center;"><a href="#"><img class="logo-footer" src="http://preprod.itsabeautifulway.com/russinvecchi.com/wp-content/uploads/logo.png" alt=""></a></p>
+			<p style="text-align: center;">
+				Russin & Vecchi was founded in Asia over 50 years ago to serve emerging economies.
+				From entry strategy to operations, we provide creative and alternative legal, business solutions in many fields. We bring practical and commercial answers to our clients.
+			</p>
+			<p style="text-align: center;font-size:8px">
+				All Rights Reserved. &copy; Russin & Vecchi 2020 | Website by Digital Mekong
+			</p>
 		</div>
-	</div>
-	<div class="col-white"></div>
-	<div class="col-contact ps-4">
-		<a href="javascript:void(0);" class="contact-footer mb-2">CONTACT</a>
-		<div class="address-phone-footer">
-			<p>205 Tun Hwa N. Road</p>
-			<p>9th Floor, Bank Tower</p>
-			<p>Taipei, Taiwan</p>
-			<p>T: 886-2-2712-8956/2713-6110</p>
-			<p>F: 886-2-2713-4711</p>
-			<p><a style="color:#fff" href="#">russin@russinvecchi.com.tw</a></p>
+		<div class="col-white"></div>
+		<div class="col-card ps-4 pe-4">
+			<div>
+				<p style="text-align: center;"><a href="#">DISCLAMER</a></p>
+				<p style="text-align: center;"><a href="#">PRIVATE POLICY</a></p>
+				<p style="text-align: center;"><a href="#">RELATIONSHIPS BETWEEN LAWYERS</a></p>
+			</div>
+		</div>
+		<div class="col-white"></div>
+		<div class="col-contact ps-4">
+			<a href="javascript:void(0);" class="contact-footer mb-2">CONTACT</a>
+			<div class="address-phone-footer">
+				<p>205 Tun Hwa N. Road</p>
+				<p>9th Floor, Bank Tower</p>
+				<p>Taipei, Taiwan</p>
+				<p>T: 886-2-2712-8956/2713-6110</p>
+				<p>F: 886-2-2713-4711</p>
+				<p><a href="#">russin@russinvecchi.com.tw</a></p>
+			</div>
 		</div>
 	</div>
 </footer>
